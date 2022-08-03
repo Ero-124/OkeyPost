1863cbbe6daa95c8213e89907f8ffc3b767c6662
unknown <khachatryanero.2002@mail.ru>
2022-08-03 19:42:44 +0400

    new

--- js/main.js
+++ js/main.js
@@ -3,17 +3,17 @@ const toggle = document.getElementById("toggle");
 const sidebar = document.getElementById("sidebar");
 const body = document.body;
 document.onclick = function (e) {
-	if (e.target !== toggle && e.target.id !== sidebar) {
+	if (e.target !== toggle && e.target !== sidebar) {
 		toggle.classList.remove("active");
 		sidebar.classList.remove("active");
 		body.classList.remove("lock")
 	}
+};
 
-	toggle.onclick = function () {
-		toggle.classList.toggle("active");
-		sidebar.classList.toggle("active");
-		body.classList.toggle("lock")
-	};
+toggle.onclick = function () {
+	toggle.classList.toggle("active");
+	sidebar.classList.toggle("active");
+	body.classList.toggle("lock")
 };
 
 
@@ -69,18 +69,66 @@ for (let i = 0; i < item.length; i++) {
 
 
 
-
-
+const popupBg = document.querySelector(".popup-account__bg");
 
 
 const openPopup = document.querySelector(".right-menu__login");
-const popup = document.querySelector(".popup-account__container");
 const closeBtn = document.querySelector(".popup-account__close");
+
+openPopup.addEventListener("click", function () {
+	popup.classList.add("show");
+	document.body.classList.add("active");
+	popupBg.classList.add("active");
+})
+
+closeBtn.addEventListener("click", function () {
+	popup.classList.remove("show");
+	document.body.classList.remove("active");
+	popupBg.classList.remove("active");
+})
+
+
+
+
+
+
+const signin = document.querySelector(".popup-account__signin-link");
+const signin2 = document.querySelector(".popup-account__signin-lnk");
+const popup = document.querySelector(".popup-account__container");
+const recovery = document.querySelector(".popup-account__recovery-link");
+const login = document.querySelector(".popup-account__login-link");
+
+
+recovery.addEventListener("click", function () {
+	document.querySelector(".popup-account__recov").style.display = "block";
+	document.querySelector(".popup-account__signin").style.display = "none";
+	document.querySelector(".popup-account__login").style.display = "none";
+})
+
+login.addEventListener("click", function () {
+	document.querySelector(".popup-account__recov").style.display = "none";
+	document.querySelector(".popup-account__signin").style.display = "none";
+	document.querySelector(".popup-account__login").style.display = "block";
+})
+
+signin.addEventListener("click", function () {
+	document.querySelector(".popup-account__recov").style.display = "none";
+	document.querySelector(".popup-account__signin").style.display = "block";
+	document.querySelector(".popup-account__login").style.display = "none";
+})
+
+signin2.addEventListener("click", function () {
+	document.querySelector(".popup-account__recov").style.display = "none";
+	document.querySelector(".popup-account__signin").style.display = "block";
+	document.querySelector(".popup-account__login").style.display = "none";
+})
+
+
+
 const pwShowHide = document.querySelectorAll(".show-hide-pw");
 const pwFields = document.querySelectorAll(".password");
-const recovery = document.querySelector(".popup-account__recovery-link");
-const login = document.querySelector(".popup-account__login");
-const popupBg = document.querySelector(".popup-account__bg");
+
+
 
 pwShowHide.forEach(eyeIcon => {
 	eyeIcon.addEventListener("click", () => {
@@ -102,28 +150,40 @@ pwShowHide.forEach(eyeIcon => {
 	})
 });
 
-openPopup.addEventListener("click", function () {
-	popup.classList.add("show");
-	document.body.classList.add("active");
-	popupBg.classList.add("active");
+
+
+
+
+
+
+
+
+
+function openCnt(evt, info) {
+	let i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("profile-tabs__item");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = 'none';
+	}
+
+	tablinks = document.getElementsByClassName("profile-tabs__btn");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+
+	document.getElementById(info).style.display = "block";
+	evt.currentTarget.className += " active"
+
+}
+
+
+
+
+let btnEdit = document.querySelector(".btn-editing");
+let personalArea = document.querySelector(".personal-area");
+let persArea = document.querySelector(".personal-area.pers-area");
+
+btnEdit.addEventListener("click", function() {
+	btnEdit.classList.add('active');
 })
 
-
-
-closeBtn.addEventListener("click", function () {
-	popup.classList.remove("show");
-	document.body.classList.remove("active");
-	popupBg.classList.remove("active");
-})
-
-
-recovery.addEventListener("click", function () {
-	popup.classList.add("active");
-	document.querySelector(".popup-account__body").style.display = "block"
-})
-
-
-login.addEventListener("click", function () {
-	popup.classList.remove("active");
-	document.querySelector(".popup-account__body").style.display = "none"
-})
